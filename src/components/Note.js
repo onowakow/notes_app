0b8ec58f3f1c505e11c18c6a7674337815f10d19
onowakow <nowakowskiowen08@gmail.com>
2021-11-08 09:42:47 -0600

    Adding style

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,8 +3,10 @@ import React from 'react'
 const Note = ({ note, onClick }) => {
   return (
     <div>
-      <li>{note.content}</li>
-      <button value={note} onClick={onClick}>{note.important ? 'Make not important' : 'Make important'}</button>
+      <li className='note'>
+        {note.content}
+        <button className='noteButton' value={note} onClick={onClick}>{note.important ? 'Make not important' : 'Make important'}</button>
+      </li>
     </div>
   )
 }
