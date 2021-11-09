55414321526829f812ff0da78f22ace6f4fae17a
onowakow <nowakowskiowen08@gmail.com>
2021-11-09 16:15:15 -0600

    Further restructuring folder

---
+++ backend/index.js
@@ -0,0 +1,86 @@
+
+// Different syntax, but similar idea to 'import express from 'express'
+const express = require('express');
+// express() is a function, creating an express application stored in 'app'
+const app = express()
+app.use(express.json())
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello, World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  // request object (related to above URL), can be accessed with request
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log(id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(note => note.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    id: generateId,
+    date: new Date(),
+    important: body.important || false,
+    content: body.content
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server is running on port ${PORT}`);
